e3f79ef2767bd93d491813ebbe16a9378837e87d
santakurow <newtakuproj0730@gmail.com>
2020-07-31 02:52:43 +0900

    create laravel post form

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,14 +15,36 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome', ["greeting" => "Hello"]);
+  $links = \App\Link::all();  
+
+  // return view('welcome', ["links" => $links]);
+  return view("welcome")->with("links", $links)->with("greeting", "Hello");
+  // return view("welcome")->withLinks($links);
 });
 
-Route::get("/test", function () {
-  config(["app.timezone" => "America/Chicago"]);
-  $value = config("app.timezone");
-  return "Timezone is $value";
+Route::get("/submit", function() {
+    return view("submit");
 });
+
+Route::post('/submit', function (Request $request) {
+    $data = $request->validate([
+      "title" => "required|max:255",
+      "url" => "required|url|max:255",
+      "description" => "required|max:255"
+    ]);
+
+    // $link = tap(new App\Link($data))->save();
+    $link = new App\Link($data);
+    $link->save();
+
+    return redirect("/");
+});
+
+// Route::get("/test", function () {
+//   config(["app.timezone" => "America/Chicago"]);
+//   $value = config("app.timezone");
+//   return "Timezone is $value";
+// });
 
 // Route::get('/app', function () {
 //     return view("child", ["name" => "Takuya"]);
